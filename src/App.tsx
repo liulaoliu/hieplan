f681be4d6cb917c65effd45fe97860f20b4d2ffd
StalinPotato <309503745@qq.com>
2021-12-19 00:27:08 +0800

    增加了 Welcome 页

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from "react";
 import "./App.css";
+import Welcome from './Components/Welcome';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  return <div className="App">
+    <Welcome></Welcome>
+
+  </div>;
 }
 
 export default App;
---
+++ src/Components/Welcome.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+/**
+ * 
+1. 登录 btn
+    1.转向一个 登录页面
+2. 图标
+3. 背景
+
+ * 
+ * 
+ */
+
+
+export default function Welcome() {
+    return (
+        <div>
+            <button>asd</button>
+        </div>
+    )
+}
