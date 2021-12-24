9a0cf6178b469c668603c9f78a8b9b23e7d901e3
StalinPotato <309503745@qq.com>
2021-12-24 14:14:04 +0800

    POPOVER完成

--- src/Components/taskPage/needName/PopOver/PopOver.tsx
+++ src/Components/taskPage/needName/PopOver/PopOver.tsx
@@ -1,26 +1,42 @@
 import { AnyARecord } from "dns";
 import React, { ReactElement, ReactPropTypes, useState } from "react";
 import styles from "./PopOver.module.css";
-interface Props {}
+interface PopOverProps {
+  children: React.ReactNode;
+}
 
-export default function PopOver(props:any): ReactElement {
- const [status, setstate] = useState(false);
+export default function PopOver({ children }: PopOverProps): ReactElement {
+  const [status, setstate] = useState(false);
 
- const setActive =()=> setstate(true);
- const setInactive=()=> setstate(false);
-
+  const setActive = () => setstate(true);
+  const setInactive = () => setstate(false);
 
   return (
-    <div 
-    onClick={setActive}
-    className={styles.popover_container_as_place_holder}>
-      {props.children}
+    <div
+      onClick={setActive}
+      className={styles.popover_container_as_place_holder}
+    >
+      {children}
       <div
-        onClick={(e)=>{
+        onClick={(e) => {
           e.stopPropagation();
-          setInactive()
+          // console.log("popover_container Clicked(监测全局点击的utility)");
+
+          setInactive();
         }}
-      className={status===true?styles.overlay_active:styles.overlay_inactive}></div>
+        className={
+          status === true ? styles.overlay_active : styles.overlay_inactive
+        }
+      ></div>
+      <div
+        onClick={(e) => {
+          e.stopPropagation();
+          // console.log("popover的overlay_active click(子侧边栏)");
+        }}
+        className={
+          status === true ? styles.popover_active : styles.popover_inactive
+        }
+      ></div>
     </div>
   );
 }
