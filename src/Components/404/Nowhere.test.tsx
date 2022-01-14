07a1d6505fd291caa8842e9cb1762edee3387fc6
StalinPotato <309503745@qq.com>
2022-01-14 15:42:25 +0800

    继续测试

---
+++ src/Components/404/Nowhere.test.tsx
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import Nowhere from "./Nowhere";
+import React from "react";
+
+test("should render 404", () => {
+  render(<Nowhere></Nowhere>);
+  const linkElement = screen.getByText(/404/i);
+  expect(linkElement).toBeInTheDocument();
+});
--- src/Components/404/Nowhere.tsx
+++ src/Components/404/Nowhere.tsx
@@ -1,5 +1,10 @@
 import React from "react";
 
 export default function Nowhere() {
-  return <div>来到了没有知识的荒野</div>;
+  return (
+    <div>
+      <h1>404</h1>
+      <div>来到了没有知识的荒野</div>
+    </div>
+  );
 }
