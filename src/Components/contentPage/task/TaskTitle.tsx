babd6357422bce061119c2cc665e62f50ad56506
StalinPotato <309503745@qq.com>
2022-02-05 19:35:59 +0800

    注意不要使用JavaScript数组来保存 日期，可能会占用较多内存，用字符串拼接 和辅助函数替代

--- src/Components/contentPage/task/TaskTitle.tsx
+++ src/Components/contentPage/task/TaskTitle.tsx
@@ -1,11 +1,28 @@
-import React, { ReactElement } from "react";
+import React, { ReactElement, useState } from "react";
 import styles from "./TaskTitle.module.css";
+import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
 interface Props {}
 
+/**
+ * 这是 显示在task的 title 区域的 函数
+ */
 export default function TaskTitle({}: Props): ReactElement {
- 
-  
-  return <div>
-    <div className={styles.date}></div>
-  </div>;
+  //  初始化一个date实例
+  const d = new Date();
+  //  用解构赋值的方式快速获取 年月日 ,注意月 的 bizarre behavior ，不会变
+  const [year, month, day] = [d.getFullYear(), d.getMonth() + 1, d.getDate()];
+
+  let formattedDate = `${year}_${month}_${day}`;
+
+  //  这是 状态 ,会变
+  const [state, setstate] = useState(formattedDate);
+  return (
+    <div className={styles.title_container}>
+      <div className={styles.date}>{state}</div>
+      <div className={styles.change_day}>
+        <FaLongArrowAltLeft />
+        <FaLongArrowAltRight />
+      </div>
+    </div>
+  );
 }
