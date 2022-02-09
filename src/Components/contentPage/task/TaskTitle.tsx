1441764cae01ca551d7be26dfb367076bbe6a38e
StalinPotato <309503745@qq.com>
2022-02-09 21:04:51 +0800

    task的日历的 今天 逻辑 完成  霍师傅，你嘛时候是津门第一啊? 就在今天(会被点亮)

--- src/Components/contentPage/task/TaskTitle.tsx
+++ src/Components/contentPage/task/TaskTitle.tsx
@@ -1,38 +1,62 @@
 import React, { ReactElement, useState } from "react";
 import styles from "./TaskTitle.module.scss";
 import { FaLongArrowAltLeft, FaLongArrowAltRight } from "react-icons/fa";
+import FormattedDate from "./util/formattedDate";
+
 interface Props {}
 
 /**
  * 这是 显示在task的 title 区域的 函数
  */
 export default function TaskTitle({}: Props): ReactElement {
-  //  初始化一个date实例
-  const d = new Date();
-  //  用解构赋值的方式快速获取 年月日 ,注意月 的 bizarre behavior ，不会变
-  const [year, month, day] = [d.getFullYear(), d.getMonth() + 1, d.getDate()];
-
-  let formattedDate = `${year}_${month}_${day}`;
+  // 一旦初始化 通过getDate暴露的值 就不会变
+  const formattedDate = new FormattedDate().getDate();
 
   //  这是 状态 ,会变
   const [state, setstate] = useState(formattedDate);
   return (
     <div className={styles.title_container}>
-      <div className={styles.date_container }>{state}</div>
-      <div className={styles.arrow_container }>
-        <div className={styles.arrow}>
+      <div className={styles.date_container}>
+        {/* 显示date */}
+        {state}
+      </div>
+      <div className={styles.arrow_container}>
+        {/* 两个箭头 */}
+        <div
+          className={styles.arrow}
+          onClick={() => {
+            const newDate = FormattedDate.dayMinus(state);
+            setstate(newDate);
+          }}
+        >
           <FaLongArrowAltLeft />
         </div>
-        <div className={styles.arrow}>
+        <div
+          className={styles.arrow}
+          onClick={() => {
+            const newDate = FormattedDate.dayPlus(state);
+            setstate(newDate);
+          }}
+        >
           <FaLongArrowAltRight />
         </div>
       </div>
       <div className={styles.calendar_container}>
+        {/* 年月日 选项卡 */}
         <div className={styles.m2}>
-        <div className={styles.w40_border}>今天</div>
+          {/* 只在"今天"被点亮  */}
+          <div
+            className={
+              formattedDate === state
+                ? styles.w40_border_active
+                : styles.w40_border
+            }
+          >
+            今天
+          </div>
         </div>
         <div className={styles.flex_container}>
-          <div className={styles.w40_border}>日</div>
+          <div className={styles.w40_border_active}>日</div>
           <div className={styles.w40_border}>周</div>
           <div className={styles.w40_border}>月</div>
         </div>
---
+++ src/Components/contentPage/task/util/formattedDate.tsx
@@ -0,0 +1,58 @@
+/**
+ * 一个特殊的 用与在Task页面显示日期字符串的类
+ *
+ */
+export default class FormattedDate {
+  private readonly year: number;
+  private readonly month: number;
+  private readonly day: number;
+  constructor() {
+    //  初始化一个date实例
+    const d = new Date();
+    //  用解构赋值的方式快速获取 年月日 ,注意月 的 bizarre behavior
+    [this.year, this.month, this.day] = [
+      d.getFullYear(),
+      d.getMonth() + 1,
+      d.getDate(),
+    ];
+  }
+  /**
+   * 获取格式化完毕的当天日期 形式为 : "年_月_日" 比如:"2022_2_9"
+   */
+  public getDate() {
+    /**
+     * 这是 ISO 格式  也就是 YYYY-MM-DD
+     * 这个很方便的就能通过Date 转换为 date实例
+     */
+    return `${this.year}-${this.month}-${this.day}`;
+  }
+  /**
+   * 接受一个Date实例, 返回格式化的年月日
+   * 形如YYYY-MM-DD
+   */
+  private static getFormatedDateHelper(date: Date) {
+    return `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+  }
+  /**
+   * static helper
+   * 接受一个 日期 字符串，把字符串的日 项目向过去的方向 调整一天
+   * 比如 传入2022-2-9 输出 2022-2-8
+   */
+  static dayMinus(date: string): string {
+    //   包装ISO日期字符串,把日期向回拨动
+    let d = new Date(date + "Z");
+    d.setDate(d.getDate() - 1);
+    return FormattedDate.getFormatedDateHelper(d);
+  }
+  /**
+   * static helper
+   * 接受一个 日期 字符串，把字符串的日 项目向未来的方向 调整一天
+   * 比如 传入2022-2-9 输出 2022-2-10
+   */
+  static dayPlus(date: string): string {
+    //   包装ISO日期字符串,把日期向回拨动
+    let d = new Date(date + "Z");
+    d.setDate(d.getDate() + 1);
+    return FormattedDate.getFormatedDateHelper(d);
+  }
+}
