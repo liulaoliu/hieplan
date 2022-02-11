12b2361b6f1422400c167dc53812c97a85ab0bce
StalinPotato <309503745@qq.com>
2022-02-11 10:42:53 +0800

    下拉菜单开了头

--- src/Components/contentPage/task/TaskTitle.tsx
+++ src/Components/contentPage/task/TaskTitle.tsx
@@ -1,7 +1,7 @@
 import React, { ReactElement, useState } from "react";
 import styles from "./taskTitle.module.scss";
 import TaskTitleCalendar from "./taskTitleCalendar";
-
+import s from "./taskTitleDropdown.module.scss";
 /**
  * 这是 显示在task的 title 区域的 函数
  */
@@ -9,6 +9,14 @@ export default function TaskTitle(): ReactElement {
   return (
     <div className={styles.title_container}>
       <TaskTitleCalendar />
+      <div className={s.container}>
+        <div className="by_arrangement">
+          <select name="" id="">
+            <option value="1">11</option>
+            <option value="2">22</option>
+          </select>
+        </div>
+      </div>
     </div>
   );
 }
