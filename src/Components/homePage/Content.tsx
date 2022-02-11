f4c0613e418d05747a77186ea344fe3a9ecaef98
StalinPotato <309503745@qq.com>
2022-02-12 01:26:22 +0800

    样式调整，应该尽量使用vh rem而不是 px

--- src/Components/homePage/Content.tsx
+++ src/Components/homePage/Content.tsx
@@ -7,8 +7,8 @@ export default function Content(): ReactElement {
       <div className={styles.background}>
         <div className={styles.innerImage}>
           <div className={styles.content}>
-            <h1 className={styles.white}>记录个人待办事项</h1>
-            <h6 className={styles.white}>跟上时间的步伐 做时间的主人</h6>
+            <h1 className={styles.white_word}>记录个人待办事项</h1>
+            <h6 className={styles.white_word}>跟上时间的步伐 做时间的主人</h6>
           </div>
         </div>
       </div>
